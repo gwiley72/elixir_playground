a76765956c3e770451319f0c79c3f71736554835
gwiley72 <glennwiley@gmail.com>
2017-04-01 20:34:34 -0500

    added more default parameters

--- sample_enum.exs
+++ sample_enum.exs
@@ -1,4 +1,9 @@
 defmodule Sample.Enum do
-    def first([]), do: nil
-    def first([head | _]), do: head
+    def first(list, val \\ nil)
+    def first([head | _], _), do: head
+    def first([], val), do: val
+
+    def add(list, val \\ 0) do
+        [val | list]
+    end
 end
