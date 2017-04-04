cc3a5583866fae51bd9423fcf05330d196332e58
Glenn Wiley <glennwiley@gmail.com>
2017-04-03 22:45:44 -0500

    add anonymous function

--- sample_utils.exs
+++ sample_utils.exs
@@ -1,8 +1,8 @@
 defmodule Sample.Utils do
-    def square(val) do
-        val * val
-    end
-    def sum(a,b) do
-        a + b
+    def square(val), do: val * val
+    def sum(a,b), do: a + b
+
+    def custom_func(a, function) do
+        function.(a)
     end
 end
